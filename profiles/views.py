31b53ff4e88fe70af7b82b66dbff3241e9f835b4
VilmaTuresson <vilma.turesson@gmail.com>
2022-09-12 17:58:12 +0000

    create view to get profile by id and serialize data

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -13,4 +14,29 @@ class ProfileList(APIView):
         """
         profiles = Profile.objects.all()
         serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
+
+
+class ProfileDetail(APIView):
+    """
+    Profile details view
+    """
+    serializer_class = ProfileSerializer
+
+    def get_object(self, pk):
+        """
+        Function to get profile details or 404 and serialize data
+        """
+        try:
+            profile = Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        """
+        Serialize data
+        """
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile)
         return Response(serializer.data)
