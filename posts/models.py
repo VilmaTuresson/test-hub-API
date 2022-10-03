dd52fe9af689ce6379ee12dd7e10bfdcb907fa74
VilmaTuresson <vilma.turesson@gmail.com>
2022-10-03 14:48:58 +0000

    create post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    """
+    Post model
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField(max_length=2000)
+    link = models.URLField(
+        db_index=True, unique=True, blank=True
+    )
+    image = models.ImageField(
+        upload_to='images/', default='../default_post_jshpia.jpg'
+    )
+
+    class Meta:
+        """
+        Post model meta class
+        """
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.title}'
