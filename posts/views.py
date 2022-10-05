58a89c040ea99e3a437c23ce44581609667f1be8
VilmaTuresson <vilma.turesson@gmail.com>
2022-10-05 14:46:18 +0000

    make imports for post views

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Post
+from .Serializers import PostSerializer
 
-# Create your views here.
