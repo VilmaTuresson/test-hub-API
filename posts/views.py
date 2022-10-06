3d184fd450e7ff507c805894c0b4f88922fdc9b7
VilmaTuresson <vilma.turesson@gmail.com>
2022-10-06 15:30:17 +0000

    add view to get list of posts

--- posts/views.py
+++ posts/views.py
@@ -4,3 +4,17 @@ from rest_framework.views import APIView
 from .models import Post
 from .Serializers import PostSerializer
 
+
+class PostList(APIView):
+    """
+    View for list of posts
+    """
+    def get(self, request):
+        """
+        function 
+        """
+        posts = Post.object.all()
+        serializer = PostSerializer(
+            posts, many=True, context={'request': request}
+        )
+        return Response(serializer.data)
