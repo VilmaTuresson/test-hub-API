e21f4749f048ac20fcab45f53f9e4617d15389c9
VilmaTuresson <vilma.turesson@gmail.com>
2022-10-18 16:34:55 +0000

    create view for listing and creating comments

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from testhub_api.permissions import IsOwnerOrReadOnly
+from .models import Comment
+from .Serializers import CommentSerializer
 
-# Create your views here.
+
+class CommentsList(generics.ListCreateAPIView):
+    """
+    View for list of comments
+    """
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Comment.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
