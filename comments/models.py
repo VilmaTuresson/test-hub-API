5ba954a6660e94a50a0ec75554049ab3313a3410
VilmaTuresson <vilma.turesson@gmail.com>
2022-10-18 14:49:42 +0000

    create class model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Post
 
-# Create your models here.
+
+class Comment(models.Model):
+    """
+    Class for comment model
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    content = models.TextField(max_length=500)
+
+    class Meta:
+        """
+        Meta class for comment model
+        """
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.content}'
