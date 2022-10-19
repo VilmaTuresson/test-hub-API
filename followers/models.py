c67f3fed22a6e22f7a3f707e5b2cbd25d50f7413
VilmaTuresson <vilma.turesson@gmail.com>
2022-10-19 19:20:24 +0000

    create follower model

--- followers/models.py
+++ followers/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class FollowerModel(models.Model):
+    """
+    Class for follower model
+    """
+    owner = models.ForeignKey(User, related_name='following', on_delete=models.CASCADE)
+    followed = models.ForeignKey(User, related_name='followed', on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
+
+    class Meta:
+        """
+        Meta class for follower model
+        """
+        ordering = ['-created_at']
+        unique_together = ['owner', 'followed']
+
+    def __str__(self):
+        return f'{self.owner}{self.followed}'
