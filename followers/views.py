4ffb939aa132c5b26a765a5f17ab72682a875276
VilmaTuresson <vilma.turesson@gmail.com>
2022-10-20 08:28:19 +0000

    create view to list follower s

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from testhub_api.permissions import IsOwnerOrReadOnly
+from .models import Follower
+from .serializers import FollowerSerializer
 
-# Create your views here.
+
+class FollowerList(generics.ListCreateAPIView):
+    """
+    View for listing followers
+    """
+    serializer_class = FollowerSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Follower.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
