d85ffeafaa283238c90a9fd6ad9b9add74ebf95c
VilmaTuresson <vilma.turesson@gmail.com>
2022-11-10 15:16:02 +0000

    fix spelling mistake

--- testhub_api/settings.py
+++ testhub_api/settings.py
@@ -50,7 +50,7 @@ JWT_AUTH_COOKIE =  'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
 
-REST_AUTH_SERIALIZER = {
+REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'testhub_api.serializers.CurrentUserSerializer'
 }
 
