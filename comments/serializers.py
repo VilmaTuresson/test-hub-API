89436591d2769d5995cf54c785be1b3ab0ae23f2
VilmaTuresson <vilma.turesson@gmail.com>
2022-12-06 14:00:17 +0000

    add profile_img to comment serializer

--- comments/serializers.py
+++ comments/serializers.py
@@ -9,6 +9,7 @@ class CommentSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_img = serializers.ReadOnlyField(source='owner.profile.image.url')
 
     def get_is_owner(self, obj):
         """
@@ -24,7 +25,7 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
         fields = [
             'id', 'owner', 'created_at', 'content', 'post',
-            'content', 'is_owner', 'profile_id'
+            'content', 'is_owner', 'profile_id, profile_img'
         ]
 
 
